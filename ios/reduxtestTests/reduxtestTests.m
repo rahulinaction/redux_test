0dc89fa275fecabda8333a6492f5da1ac92fa4c9
rahul mendonca <rahulinaction@users.noreply.github.com>
2023-08-30 23:02:49 -0400

    Files renamed

--- ios/reduxtestTests/reduxtestTests.m
+++ ios/reduxtestTests/reduxtestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface testprojectTests : XCTestCase
+@interface reduxtestTests : XCTestCase
 
 @end
 
-@implementation testprojectTests
+@implementation reduxtestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
